0399694545839dd2768242c51c71dc80f4d690ef
ClovisYutakaHarada <ClovisYutakaHarada@DESKTOP-QNNCA83>
2018-10-23 10:53:27 -0300

    alteração campo data

--- WebApplication1core/Data/Student.cs
+++ WebApplication1core/Data/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace WebApplication1core.Data
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
